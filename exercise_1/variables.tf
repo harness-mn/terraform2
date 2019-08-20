f6def31a765d47f8e4501a81ddc7005b56457a1c
Michael Nelson <michael.nelson@harness.io>
2019-08-20 15:58:19 -0700

    Update variables.tf

--- exercise_1/variables.tf
+++ exercise_1/variables.tf
@@ -16,7 +16,7 @@ variable "s3_bucket_region" {
 variable "s3_bucket_name" {
   type        = "string"
   description = "Name of the S3 bucket to be created used to store the remote state file"
-  default     = "rc-tf-bucket-training-exersize-1-michael2"
+  default     = "rc-tf-bucket-training-exersize-1-michael3"
   }
 
 variable "tag_env" {
